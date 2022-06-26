1395db437ce610ccae67f8b7fd1b573ef4ad33fe
dilireba12 <2845414681@qq.con>
2022-06-26 18:04:28 +0800

    完成登录功能

---
+++ src/api/user.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+/**
+ * 获取短信验证码
+ * @param {number} mobile
+ * @returns promise
+ */
+export const getSmsCode = (mobile) => {
+  return request({
+    url: `/sms/codes/${mobile}`
+  })
+}
+
+/**
+ * 登录方式
+ * @param {mobile,code} param
+ * @returns promise
+ */
+export const login = ({ mobile, code }) => {
+  return request({
+    method: 'POST',
+    url: '/authorizations',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
